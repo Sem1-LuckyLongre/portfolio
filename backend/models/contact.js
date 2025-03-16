4b4f496a1ee136d78646173a431640dc3c44eee9
Sem 1 <sem1luckylongre@gmail.com>
2025-03-16 16:17:41 +0530

    backend changed

--- backend/models/contact.js
+++ backend/models/contact.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  message: String,
+  name: {
+    type: String,
+    require: true,
+  },
+  email: { type: String, require: true },
+  message: { type: String, require: true },
   createdAt: {
     type: Date,
     default: Date.now,
